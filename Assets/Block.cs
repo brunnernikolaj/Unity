0121a932310064b659d24605583391876ce66217
brunnernikolaj <brunnernikolaj@gmail.com>
2016-02-16 10:17:27 +0100

    initial

---
+++ Assets/Block.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        transform.DOScale(0f, 0.5f);
+        Destroy(gameObject, 0.5f);
+        GameMaster.instance.BlockDestroyed();
+    }
+}
---
+++ Assets/GameMaster.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster instance;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public int bricks;
+
+    private GameMaster()
+    {
+
+    }
+
+    public void Die()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void BlockDestroyed()
+    {
+        if (--bricks == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
